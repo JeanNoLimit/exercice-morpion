814e47e3a917d0a4d6bd121c36c24085eda85bde
Jean-Noel Richert <jean.noel.richert@gmail.com>
2023-03-30 09:51:29 +0200

    Ajout message partie terminée

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 // Création d'un élément carré
-const carre=document.createElement("div");
+const carre = document.createElement("div");
 carre.classList.add("carre");
-const message=document.createElement("p");
+const message = document.createElement("p");
 message.classList.add("message");
-const container=document.querySelector("#container");
-const morpion=document.querySelector("#morpion");
+const container = document.querySelector("#container");
+const morpion = document.querySelector("#morpion");
 container.appendChild(message);
-let cases=[] //Tableau contenant nos div, 
+let cases = [] //Tableau contenant nos div, 
 
-for(let i=1; i<=9; i++){
-
-    let newCarre=carre.cloneNode();
-    morpion.appendChild(newCarre); 
+for (let i = 1; i <= 9; i++) {
+    let newCarre = carre.cloneNode();
+    morpion.appendChild(newCarre);
     cases.push(newCarre);
-    
-;}
-// console.log(cases);
+    ;
+}
 
+let nbChecked = 1;
 
-let nbChecked =0;
 //On part du principie qu'une partie débute par X
-message.innerHTML="C'est au tour de X!";
+message.innerHTML = "C'est au tour de X!";
 let tour = 1; //défini si X ou O doit jouer
-    cases.forEach(newCarre => {
-        newCarre.addEventListener("click", (e)=> {
-            //On vérifie que la l'élément newCarre n'appartient pas déja à une des 2 classes "clicked"
-        if(newCarre.classList.contains("x-clicked")||newCarre.classList.contains("o-clicked")){
-            message.innerHTML="case déja jouée!";
-        }else{
+cases.forEach(newCarre => {
+    newCarre.addEventListener("click", (e) => {
+
+        //On vérifie que la l'élément newCarre n'appartient pas déja à une des 2 classes "clicked https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/"
+        if (newCarre.classList.contains("x-clicked") || newCarre.classList.contains("o-clicked")) {
+            alert("case déja jouée!");
+        } else {
+            console.log(nbChecked);
             nbChecked++;
-            if(tour==1){
-                newCarre.innerHTML="X";
+            if (tour == 1) {
+                newCarre.innerHTML = "X";
                 newCarre.classList.add("x-clicked")
-                message.innerHTML="C'est au tour de O!"
-                tour=-1;
-                // e.removeEventListener("click");
-            
-            }else {
-                newCarre.innerHTML="O";
+                message.innerHTML = "C'est au tour de O!"
+                tour = -1;
+                // e.removeEventListener("click"); solution alternative, il faudrait créer une fonction click pour pouvoir retirer l'event. https://stackoverflow.com/questions/4402287/javascript-remove-event-listener
+            } else {
+                newCarre.innerHTML = "O";
                 newCarre.classList.add("o-clicked")
-                message.innerHTML="C'est au tour de X!"
-                tour=1;
+                message.innerHTML = "C'est au tour de X!"
+                tour = 1;
+
             }
         }
-        });
-    });
+        if (nbChecked > 9) {
+            message.innerHTML = ("Partie terminée");
+        }
 
+    });
+});
